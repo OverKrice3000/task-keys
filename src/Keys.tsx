f1cab6ef16169de84cfa83e8af4d57750a2f05aa
OverKrice3000 <bober.xxl@yandex.ru>
2022-06-05 14:33:17 +0700

    uploading

--- src/Keys.tsx
+++ src/Keys.tsx
@@ -1,5 +1,82 @@
 import { IItem } from './index';
+import { useCallback, useState } from 'react';
+
+function Key(props: {
+    id: number;
+    name: string;
+    edit: boolean;
+    curCb: any;
+    elemCb: any;
+}) {
+    // How to get input value if there is no state
+    // How to connect element with state
+    // How to set onclick (event listener) to react element
+    // Key why
+    // How to know state of child / input value (DOM?)
+
+    let [elName, setELName] = useState(props.name);
+
+    if (props.edit) {
+        return (
+            <input
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                        props.elemCb(props.id, elName);
+                        props.curCb(-1);
+                    } else if (e.key === 'Escape') {
+                        props.curCb(-1);
+                    }
+                }}
+                value={elName}
+                onChange={(e) => {
+                    setELName(e.target.value);
+                }}
+            />
+        );
+    }
+    return (
+        <li
+            onClick={(e) => {
+                props.curCb(props.id);
+            }}
+        >
+            {props.name}
+        </li>
+    );
+}
 
 export function Keys(props: { initialData: IItem[]; sorting: 'ASC' | 'DESC' }) {
-    return <div></div>;
+    const [data, setData] = useState(props.initialData);
+    const [curEdit, setCurEdit] = useState(-1);
+    const changeCurEdit = useCallback((cur: number) => {
+        setCurEdit(cur);
+    }, []);
+    const changeElement = useCallback((id: number, name: string) => {
+        let index = data.findIndex((value) => {
+            return id === value.id;
+        });
+        data[index].name = name;
+    }, []);
+    // Callback to change element. Convey to key and call in use effect
+    // How to change an element of array
+    // When does render
+    // When state changes is there render
+    let sorted: IItem[] = data.sort();
+    sorted = props.sorting === 'DESC' ? sorted.reverse() : sorted;
+    return (
+        <div>
+            {sorted.map((item) => {
+                return (
+                    <Key
+                        key={item.id}
+                        id={item.id}
+                        name={item.name}
+                        edit={curEdit === item.id}
+                        curCb={changeCurEdit}
+                        elemCb={changeElement}
+                    />
+                );
+            })}
+        </div>
+    );
 }
